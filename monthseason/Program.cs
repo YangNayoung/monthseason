31cb552a483d420b9d4bd8da5d946f5819e2e8d1
Nayoung <s2018s32@e-mirim.hs.kr>
2020-05-06 23:25:51 +0900

    실습6 (2020.05.06)

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,6 +10,37 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.Write("월 입력: ");
+            var month = Console.ReadLine();
+
+            int number = 0;
+            if (!(month.Contains("월") || int.TryParse(month, out number)))
+            {
+                Console.WriteLine("잘못입력하셨습니다!");
+            }
+            else
+            {
+                if (month.Contains("3") || month.Contains("4") || month.Contains("5"))
+                {
+                    Console.WriteLine("봄");
+                }
+                else if (month.Contains("6") || month.Contains("7") || month.Contains("8"))
+                {
+                    Console.WriteLine("여름");
+                }
+                else if (month.Contains("9") || month.Contains("10") || month.Contains("11"))
+                {
+                    Console.WriteLine("가을");
+                }
+                else if (month.Contains("12") || month.Contains("1") || month.Contains("2"))
+                {
+                    Console.WriteLine("겨울");
+                }
+                else
+                {
+                    Console.WriteLine("잘못입력하셨습니다!");
+                }
+            }
         }
     }
 }
